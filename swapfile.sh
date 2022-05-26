20464052ba9a6b32b0f52e34be7fe84ad7863961
Sugato Sengupta <thcdesigning@gmail.com>
2022-05-26 17:42:11 +0530

    Update swapfile.sh

--- swapfile.sh
+++ swapfile.sh
@@ -2,18 +2,18 @@
 
 if [ -e  /swapfile ];
 then
-echo "SWAP Already exists. Quitting" && exit 0
+echo "Swap Already exists. Quitting." && exit 0
 else
-	echo "How much memory to add (in GB)?"
+	echo "How much memory to add (number, in GB)?"
 	read MEMORY
 	allocation="${MEMORY}G"
-	sudo fallocate -l $allocation /swapfile && echo "allocation done"
+	sudo fallocate -l $allocation /swapfile && echo "Allocation done."
 	sudo chmod 600 /swapfile
 	sudo mkswap /swapfile
 	sudo swapon /swapfile
 	fstab=/etc/fstab
 	if grep -q "swapfile" $fstab; then
-		echo "Swapfile is already there in  fstab"
+		echo "Swapfile is already there in fstab."
 	else
 		echo  "/swapfile swap swap defaults 0 0"  |   sudo tee -a $fstab
 	fi
